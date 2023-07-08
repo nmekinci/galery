2af30894bce335fbe2b5fa2fe7ce09f8dd04caaa
Must <emailme>
2023-07-08 13:14:06 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Homepage from './pages/Homepage';
+// import "./styles/styles.css"
+import "./styles/style.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gallery</h1>
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,22 @@
+import Picture from "../components/Picture";
+import data from "../helper/data"
+
+
+const Homepage = () => {
+    // console.log(data);
+    // const [photografer, src] = data
+    // console.log(photografer,src);
+    
+  return (
+
+    <div className="pictures">
+        {data.map( (item,i) => (
+        <Picture key={i} photo={item.photographer} img={item.src}/>
+        // console.log(item);
+    ))}
+    
+    </div>
+  )
+}
+
+export default Homepage
